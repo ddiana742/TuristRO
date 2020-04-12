fba87c06831028c989a0866c875473a56ed59c24
MB <MB@MB-PC>
2020-04-12 17:15:52 +0300

    first commit

---
+++ MasterDetail/MasterDetail/Meniu/Orase.xaml.cs
@@ -0,0 +1,52 @@
+﻿using MasterDetail.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MasterDetail
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Cities2()); //hunedoara
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Cities3()); //brasov
+        }
+
+        private async void Button_Clicked_2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new City4()); //suceava
+        }
+
+        private async void Button_Clicked_3(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new City5()); //cluj
+        }
+
+        private async void Button_Clicked_4(object sender, EventArgs e)
+        {
+             await Navigation.PushAsync(new City6()); //constanta
+        }
+
+        private async void Button_Clicked_5(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AlbaIulia());
+        }
+        private async void OnImageNameTapped(object sender, EventArgs args) => await Navigation.PushAsync(new AlbaIulia());
+    }
+}
